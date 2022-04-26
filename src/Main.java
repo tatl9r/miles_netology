472c19a86116201fb421c54e76d2077bd99e458f
tatl9r <e.shuvaeva@outlook.com>
2022-04-26 20:39:13 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 30_000;
+        int mile = 20;
+        int bonus = amount / mile;
+        System.out.println("Бонус: " + bonus);
+    }
+}
